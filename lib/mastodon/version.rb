d878b21c5cf41a1b1ce5844b35946cf5163afe1a
hinusif <161274975+hinusif@users.noreply.github.com>
2024-08-12 23:14:56 +0900

    Bump revision to 6

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      ['r5', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
+      ['r6', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
     end
 
     def to_a
