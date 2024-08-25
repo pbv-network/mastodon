93c67218a4e7527481b13c3a8ec07b16fcb21d44
hinusif <161274975+hinusif@users.noreply.github.com>
2024-08-26 00:29:30 +0900

    Bump revision to r8

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      ['r7', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
+      ['r8', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
     end
 
     def to_a
