29ebee1220e23a1fb6ab50ce4db8ee1400e17d58
hinusif <hinusif@proton.me>
2024-10-26 01:00:09 +0000

    Change repository link to PBVN Mastodon repository

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -50,7 +50,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'pbv-network/mastodon')
     end
 
     def source_base_url
