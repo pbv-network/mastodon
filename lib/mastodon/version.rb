04e12ca14d301116aaae291b45dd6321eeb8594e
hinusif <hinusif@proton.me>
2024-10-26 00:59:28 +0000

    Add PBVN custom revision number

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -25,7 +25,7 @@ module Mastodon
     end
 
     def build_metadata
-      ENV.fetch('MASTODON_VERSION_METADATA', nil)
+      ['r8', ENV.fetch('MASTODON_VERSION_METADATA', nil)].compact_blank.join('.')
     end
 
     def to_a
