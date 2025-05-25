356279ba7ca689d54f94792d847117e1de693975
hinusif <hinusif@proton.me>
2025-05-25 19:43:38 +0000

    Squashed commit of the following:
    commit dcf2a0d87e7b7725c14e4e62ca923b73cfe1d144
    Author: hinusif <hinusif@proton.me>
    Date:   Sat Oct 26 01:32:44 2024 +0000
    
        Increase username length and display limit to 100

--- app/models/account.rb
+++ app/models/account.rb
@@ -72,8 +72,8 @@ class Account < ApplicationRecord
   MENTION_RE    = %r{(?<![=/[:word:]])@((#{USERNAME_RE})(?:@[[:word:]]+([.-]+[[:word:]]+)*)?)}
   URL_PREFIX_RE = %r{\Ahttp(s?)://[^/]+}
   USERNAME_ONLY_RE = /\A#{USERNAME_RE}\z/i
-  USERNAME_LENGTH_LIMIT = 30
-  DISPLAY_NAME_LENGTH_LIMIT = 30
+  USERNAME_LENGTH_LIMIT = 100
+  DISPLAY_NAME_LENGTH_LIMIT = 100
   NOTE_LENGTH_LIMIT = 500
 
   AUTOMATED_ACTOR_TYPES = %w(Application Service).freeze
