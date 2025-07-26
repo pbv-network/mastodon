fae212999db0a9772ab6c71e3548f51799d3a7c3
hinusif <hinusif@proton.me>
2025-07-26 05:52:18 +0000

    Change default to expanded for server rules section

--- app/javascript/mastodon/features/about/components/rules.tsx
+++ app/javascript/mastodon/features/about/components/rules.tsx
@@ -47,12 +47,12 @@ export const RulesSection: FC<RulesSectionProps> = ({ isLoading = false }) => {
   );
 
   if (isLoading) {
-    return <Section title={intl.formatMessage(messages.rules)} />;
+    return <Section open title={intl.formatMessage(messages.rules)} />;
   }
 
   if (rules.length === 0) {
     return (
-      <Section title={intl.formatMessage(messages.rules)}>
+      <Section open title={intl.formatMessage(messages.rules)}>
         <p>
           <FormattedMessage
             id='about.not_available'
@@ -64,7 +64,7 @@ export const RulesSection: FC<RulesSectionProps> = ({ isLoading = false }) => {
   }
 
   return (
-    <Section title={intl.formatMessage(messages.rules)}>
+    <Section open title={intl.formatMessage(messages.rules)}>
       <ol className='rules-list'>
         {rules.map((rule) => (
           <li key={rule.id}>
